8eb297541201cc7cdd57033eaf157bf23a15e656
Ernesto Pizarro <epizarro@certant.com>
2024-03-06 13:17:51 -0300

    validation

--- src/main/java/com/example/msscbeerservice/model/BeerDTO.java
+++ src/main/java/com/example/msscbeerservice/model/BeerDTO.java
@@ -1,5 +1,9 @@
 package com.example.msscbeerservice.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,20 +19,27 @@ import java.util.UUID;
 @Builder
 public class BeerDTO {
 
+    @Null
     private UUID id;
-
+    @Null
     private Integer version;
-
+    @Null
     private OffsetDateTime createdDate;
-
+    @Null
     private OffsetDateTime lastModifiedDate;
 
+    @NotBlank
     private String beerName;
 
+    @NotNull
     private BeerStyleEnum beerStyle;
 
+    @NotNull
+    @Positive
     private Long upc;
 
+    @NotNull
+    @Positive
     private BigDecimal price;
 
     private Integer quantityOnHand;
